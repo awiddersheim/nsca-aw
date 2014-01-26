c23722e5f5ac3adc9d80c44ca71e0cce20d15a0f
Andrew Widdersheim <awiddersheim@inetu.net>
2014-01-26 09:20:28 -0500

    Second pass on changing code style for netutils.c

--- src/netutils.c
+++ src/netutils.c
@@ -57,10 +57,10 @@ int my_connect(char *host_name, int port, int *sd, char *proto) {
 	servaddr.sin_port=htons(port);
 
 	/* try to bypass using a DNS lookup if this is just an IP address */
-	if(!my_inet_aton(host_name,&servaddr.sin_addr)) {
+	if (!my_inet_aton(host_name,&servaddr.sin_addr)) {
 		/* else do a DNS lookup */
 		hp=gethostbyname((const char *)host_name);
-		if(hp==NULL) {
+		if (hp==NULL) {
 			printf("Invalid host name '%s'\n", host_name);
 			return(STATE_UNKNOWN);
 		}
@@ -69,7 +69,7 @@ int my_connect(char *host_name, int port, int *sd, char *proto) {
 	}
 
 	/* map transport protocol name to protocol number */
-	if(((ptrp=getprotobyname(proto))) == NULL) {
+	if (((ptrp=getprotobyname(proto))) == NULL) {
 		printf("Cannot map \"%s\" to protocol number\n", proto);
 		return(STATE_UNKNOWN);
 	}
@@ -80,14 +80,14 @@ int my_connect(char *host_name, int port, int *sd, char *proto) {
 		(!strcmp(proto, "udp")) ? SOCK_DGRAM : SOCK_STREAM, ptrp->p_proto
 	);
 
-	if(*sd < 0){
+	if (*sd < 0){
 		printf("Socket creation failed\n");
 		return(STATE_UNKNOWN);
 	}
 
 	/* open a connection */
 	result = connect(*sd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-	if(result < 0) {
+	if (result < 0) {
 		switch(errno) {
 			case ECONNREFUSED:
 				printf("Connection refused by host\n");
@@ -127,22 +127,24 @@ int my_inet_aton(register const char *cp, struct in_addr *addr) {
 		 */
 		if (!isdigit((int)c))
 			return(0);
-		val=0; base=10;
 
-		if( c== '0') {
+		val=0;
+		base=10;
+
+		if ( c== '0') {
 			c = *++cp;
-			if(c == 'x' || c == 'X')
+			if (c == 'x' || c == 'X')
 				base = 16, c = *++cp;
 			else
 				base = 8;
 		}
 
 		for(;;) {
-			if(isascii((int)c) && isdigit((int)c)) {
+			if (isascii((int)c) && isdigit((int)c)) {
 				val=(val*base) + (c -'0');
 				c = *++cp;
 			}
-			else if(base == 16 && isascii((int)c) && isxdigit((int)c)) {
+			else if (base == 16 && isascii((int)c) && isxdigit((int)c)) {
 				val = (val << 4) | (c + 10 - (islower((int)c) ? 'a' : 'A'));
 				c = *++cp;
 			}
@@ -150,14 +152,14 @@ int my_inet_aton(register const char *cp, struct in_addr *addr) {
 				break;
 		}
 
-		if(c == '.') {
+		if (c == '.') {
 			/*
 			 * Internet format:
 			 *	a.b.c.d
 			 *	a.b.c	(with c treated as 16 bits)
 			 *	a.b	(with b treated as 24 bits)
 			 */
-			if(pp >= parts + 3)
+			if (pp >= parts + 3)
 				return(0);
 			*pp++ = val;
 			c = *++cp;
@@ -167,7 +169,7 @@ int my_inet_aton(register const char *cp, struct in_addr *addr) {
 		}
 
 	/* Check for trailing characters */
-	if(c != '\0' && (!isascii((int)c) || !isspace((int)c)))
+	if (c != '\0' && (!isascii((int)c) || !isspace((int)c)))
 		return(0);
 
 	/* Concoct the address according to the number of parts specified */
@@ -178,23 +180,23 @@ int my_inet_aton(register const char *cp, struct in_addr *addr) {
 		case 1:				/* a -- 32 bits */
 			break;
 		case 2:				/* a.b -- 8.24 bits */
-			if(val > 0xffffff)
+			if (val > 0xffffff)
 				return(0);
 			val|=parts[0]<<24;
 			break;
 		case 3:				/* a.b.c -- 8.8.16 bits */
-			if(val>0xffff)
+			if (val>0xffff)
 				return(0);
 			val|=(parts[0]<< 24) | (parts[1]<<16);
 			break;
 		case 4:				/* a.b.c.d -- 8.8.8.8 bits */
-			if(val>0xff)
+			if (val>0xff)
 				return(0);
 			val|=(parts[0]<<24) | (parts[1]<<16) | (parts[2]<<8);
 			break;
 	}
 
-	if(addr)
+	if (addr)
 		addr->s_addr=htonl(val);
 
 	return(1);
@@ -208,7 +210,7 @@ int sendall(int s, char *buf, int *len){
 
 	while(total < *len) {
 		n = send(s, buf + total, bytesleft, 0);
-		if(n == -1)
+		if (n == -1)
 			break;
 		total += n;
 		bytesleft -= n;
@@ -240,15 +242,15 @@ int recvall(int s, char *buf, int *len, int timeout){
 		n = recv(s, buf + total, bytesleft, 0);
 
 		/* no data has arrived yet (non-blocking socket) */
-		if(n == -1 && errno == EAGAIN) {
+		if (n == -1 && errno == EAGAIN) {
 			time(&current_time);
-			if(current_time-start_time>timeout)
+			if (current_time-start_time>timeout)
 				break;
 			sleep(1);
 			continue;
 		}
 		/* receive error or client disconnect */
-		else if(n <= 0)
+		else if (n <= 0)
 			break;
 
 		/* apply bytes we received */
